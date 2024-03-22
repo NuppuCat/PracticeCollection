2df32b5b788b4395ae01f71070eeac921d782a23
NuppuCat <161198370+NuppuCat@users.noreply.github.com>
2024-03-22 15:10:17 +0200

    Delete Practice_at_university/COMP.SGN.110 Image intro directory

--- Practice_at_university/COMP.SGN.110 Image intro/week1/ex1/getnum.m
+++
@@ -1,7 +0,0 @@
-function num = getnum(S)
-b = (S~=0);
-% expand
-num = sum(b(:));
-
-%sum(b(:))
-end
--- Practice_at_university/COMP.SGN.110 Image intro/week2/ex3/ContrastStretch.m
+++
@@ -1,6 +0,0 @@
-function I2 = ContrastStretch(I)
-m = max(I(:));
-mi = min(I(:));
-I2 = (I-mi)*(255/(m-mi));
-
-end
