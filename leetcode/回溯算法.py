0e553cdabbc377c1b4c962167a8139932c1f573c
NuppuCat <161198370+NuppuCat@users.noreply.github.com>
2024-09-06 12:41:11 +0300

    Update 回溯算法.py

--- leetcode/回溯算法.py
+++ leetcode/回溯算法.py
@@ -30,3 +30,104 @@ class Solution(object):
         """
         self.sbt(n,k,1)
         return self.r
+
+
+#优化横向循环提升效率
+
+class Solution(object):
+    def __init__(self):
+        self.q = []
+        self.r = []
+    def sbt(self,n,k,id):
+        if len(self.q)==k:
+            self.r.append(self.q[:])
+            return
+	#主要思想是看剩余长度n-i 是否够所需的长度k-len(q)
+#得到关于i的不等式。重定边界+2
+        for i in range(id,n-(k-len(self.q))+2):
+            
+            self.q.append(i)
+            self.sbt(n,k,i+1)
+            self.q.pop()
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        self.sbt(n,k,1)
+        return self.r
+
+#216.组合总和III https://leetcode.cn/problems/combination-sum-iii/
+#这就多了点条件，使用剪枝就可以解决
+class Solution(object):
+    def sbt(self,k,n,q,r,s):
+        if sum(q)>n or len(q)>k:
+            return
+        if len(q)==k:
+            if sum(q)==n:
+                r.append(q[:])
+            else:
+                return
+        
+        for i in range(s,9 - (k - len(q)) + 2):
+            if i in q:
+                continue
+            q.append(i)
+            self.sbt(k,n,q,r,i+1)
+            q.pop()
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        q = []
+        r = []
+        self.sbt(k,n,q,r,1)
+        return r
+
+
+#17.电话号码的字母组合 https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+#这里主要是建一个数组来存储数字对应的字母，以方便查表
+#剩余思想和组合一致
+class Solution(object):
+    def __init__(self):
+        self.letterMap = [
+            "",     # 0
+            "",     # 1
+            "abc",  # 2
+            "def",  # 3
+            "ghi",  # 4
+            "jkl",  # 5
+            "mno",  # 6
+            "pqrs", # 7
+            "tuv",  # 8
+            "wxyz"  # 9
+        ]
+        self.result = []
+        self.s = ""
+    def sbt(self,digits,start):
+        if len(self.s)==len(digits):
+		#这里由于字符串存储在底层而非对象，所以可以直接append
+            self.result.append(self.s)
+            return
+	#注意这里digits是字符串，因此要转换成int才能作为index
+        i  = int(digits[start])
+        for j in self.letterMap[i]:
+         
+            self.s+=j
+            self.sbt(digits,start+1)
+		#巧用[:-1]删除最后一位字符
+            self.s =self.s[:-1]
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+	#这里需要判空，否则空组中会放一个空字符串
+        if not digits:
+            return []
+        self.sbt(digits,0)
+        return self.result
