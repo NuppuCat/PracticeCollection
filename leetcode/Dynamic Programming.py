34be221213139b7012f9c07092c5b70b8febcfba
NuppuCat <161198370+NuppuCat@users.noreply.github.com>
2024-10-13 10:37:16 +0300

    Update Dynamic Programming.py

--- leetcode/Dynamic Programming.py
+++ leetcode/Dynamic Programming.py
@@ -17,3 +17,23 @@ class Solution(object):
         for i in range(2,n+1):
             F[i] = F[i-1]+F[i-2]
         return F[n]
+
+
+#70. 爬楼梯  https://leetcode.cn/problems/climbing-stairs/submissions/572335795/
+#这问题本质是斐波那契数列，但是为什么需要思考
+#那就是到第i层，因为步长有两种，所以有两种情况，一种是从i-1上1层
+#另一种是从i-2上两层
+#而上到 i-1 和 i-2 层的方法和被记到了数组中
+#因此到第i 层共有 r[i]  = r[i-1]+r[i-2] 种方法
+#明白了这一点就很简单了，而因为n=2 r=2,因此初始化数组为1。虽然n = 0无意义
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        r = [1]*(n+1)
+        
+        for i in range(2,n+1):
+            r[i]  = r[i-1]+r[i-2]
+        return r[n]
