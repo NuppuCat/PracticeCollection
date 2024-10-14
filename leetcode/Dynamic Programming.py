c99b74cddd5d296b85605130f602cfd7b03158a5
NuppuCat <161198370+NuppuCat@users.noreply.github.com>
2024-10-14 10:47:55 +0300

    Update Dynamic Programming.py

--- leetcode/Dynamic Programming.py
+++ leetcode/Dynamic Programming.py
@@ -37,3 +37,35 @@ class Solution(object):
         for i in range(2,n+1):
             r[i]  = r[i-1]+r[i-2]
         return r[n]
+
+#746. 使用最小花费爬楼梯 https://leetcode.cn/problems/min-cost-climbing-stairs/submissions/572592459/
+#总体思路是，用动态数组记录到达第i阶的最小花费
+#那么每一阶的最小花费就是到达 前一阶 和 前两阶 的花费，加上它们相对应的花费
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+	#0，1可以直接跨过，所以从2开始遍历.即第一步无消耗
+        r = [0] * (len(cost)+1)
+        
+        for i in range(2,len(cost)+1):
+            r[i]  = min(r[i-2]+cost[i-2],r[i-1]+cost[i-1])
+        return r[-1]
+
+#这个解是最开始写出来的
+#第一步有消耗，最后一步无消耗
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        r = [0] * (len(cost)+1)
+        r[1] = cost[0]
+        for i in range(2,len(cost)+1):
+            r[i]  = min(r[i-2]+cost[i-1],r[i-1]+cost[i-1])
+	#到达倒数第一二阶，比较大小
+        return min(r[-1],r[-2])
